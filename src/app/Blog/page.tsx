99a6c278f437b1cebf058ea6806ff14a7b90176b
Murunwa <134610001+MurunwaMaphiri1@users.noreply.github.com>
2025-10-18 00:07:35 +0200

    Minor changes

--- src/app/Blog/page.tsx
+++ src/app/Blog/page.tsx
@@ -9,7 +9,6 @@ type BlogItem = {
     imageSrc: string;
     title: string;
     Description: string;
-    tags: string[];
     DateCreated: Date;
     path: string;
 }
@@ -17,20 +16,20 @@ type BlogItem = {
 const blogItems: BlogItem[] = [
     {
         id: 1,
-        imageSrc: "/Images/image_22531_3_normal.jpg",
-        title: "Abraham's Altars: A Study in Faith",
+        imageSrc: "/Images/202020169_univ_cnt_1_xl.jpg",
+        title: "Joseph's story: Having Faith that God will fulfill His promises",
         Description: "An exploration of the significant altars built by Abraham and what they represent in his spiritual journey.",
-        tags: [
-            "Christianity",
-            "Old Testament",
-            "Faith"
-        ],
-        DateCreated: new Date("2024-10-01T10:00:00Z"),
+        DateCreated: new Date("2024-10-16T10:00:00Z"),
         path: "/blog/abrahams-altars"
     }
 ]
 
 export default function Blogs() {
+
+    const sortedBlogItems = blogItems.slice().sort((a, b) => 
+        b.DateCreated.getTime() - a.DateCreated.getTime()
+    );
+
     return (
         <>
             <div className="flex flex-col gap-20 p-7 max-w-3xl mx-auto">
@@ -42,13 +41,12 @@ export default function Blogs() {
                         just jotting down my thoughts while doing bible study
                     </p>
                 </div>
-                {blogItems.map((blog, index) => (
+                {sortedBlogItems.map((blog, index) => (
                     <BlogCard
                         key={blog.id}
                         imageSrc={blog.imageSrc}
                         title={blog.title}
                         Description={blog.Description}
-                        tags={blog.tags}
                         delay={index * 0.2}
                         DateCreated={blog.DateCreated}
                         path={blog.path}
--- src/components/Blog/BlogCard.tsx
+++ src/components/Blog/BlogCard.tsx
@@ -3,11 +3,12 @@ import Link from "next/link";
 import React from "react";
 import { ArrowRight } from 'lucide-react';
 
+//text-x1
+
 interface BlogCardProps {
     imageSrc: string;
     title: string;
     Description: string;
-    tags: string[];
     DateCreated: Date;
     delay: number
     path: string;
@@ -17,40 +18,29 @@ const BlogCard: React.FC<BlogCardProps> = ({
     imageSrc,
     title,
     Description,
-    tags,
     DateCreated,
     path,
 }) => {
     return (
         <>
-            <div className="flex flex-col gap-10 sm:flex-row">
-                <div className="flex h-56 justify-center overflow-hidden rounded-lg">
+            <div className="flex flex-col gap-5 sm:flex-row">
+                <div className="flex h-56 justify-center overflow-hidden">
                     <Image
                     width={1536}
                     height={1000}
                     alt={`${title} blog image`}
-                    className="size-full rounded-lg object-cover transition-all duration-300 hover:scale-105"
+                    className="size-full object-cover transition-all duration-300 hover:scale-105"
                     aria-label={`${title} blog image`}
                     src={imageSrc}
                     />
                 </div>
-                <div className="flex-1 space-y-3 p-4">
-                    <h2 className="mb-1 text-x1 font-semibold dark:text-white">
+                <div className="flex-1 space-y-3">
+                    <h2 className="mb-1 text-[19px] font-semibold dark:text-white">
                         {title}
                     </h2>
-                    <p className="mb-4 text-xs dark:text-slate-300">
+                    <p className="mb-4 text-[13px] dark:text-slate-300">
                         {Description}
                     </p>
-                    <div className="flex flex-wrap gap-2">
-                        {tags.map((tag, index) => (
-                            <div
-                                className="rounded-lg bg-white text-sm font-semibold text-black p-1"
-                                key={index}
-                            >
-                                {tag}
-                            </div>
-                        ))}
-                    </div>
                     <div className="flex gap-4">
                         <Link
                             href={path}
