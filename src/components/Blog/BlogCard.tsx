29530d42c36ff224f210b19cb89e99e60b82f96d
Murunwa <134610001+MurunwaMaphiri1@users.noreply.github.com>
2025-10-22 00:05:08 +0200

    Minor UI Changes

--- src/components/Blog/BlogCard.tsx
+++ src/components/Blog/BlogCard.tsx
@@ -3,8 +3,6 @@ import Link from "next/link";
 import React from "react";
 import { ArrowRight } from 'lucide-react';
 
-//text-x1
-
 interface BlogCardProps {
     imageSrc: string;
     title: string;
